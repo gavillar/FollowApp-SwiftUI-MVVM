c0dfda9efa545eda7df04089016e3aa6748b9b59
VillarDeveloper <villar.developer@gmail.com>
2023-01-21 06:00:30 -0800

    Initial Commit

---
+++ follow/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  follow
+//
+//  Created by user220831 on 1/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ follow/followApp.swift
@@ -0,0 +1,17 @@
+//
+//  followApp.swift
+//  follow
+//
+//  Created by user220831 on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct followApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
